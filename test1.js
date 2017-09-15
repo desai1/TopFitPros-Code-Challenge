b2b8621b3bf665f9ee5a2afeafb18b650a37ad6f
ishit desai <desai_ishit@yahoo.com>
2017-09-15 19:04:22 -0400

    Solved First Challenge

--- test1.js
+++ test1.js
@@ -8,5 +8,35 @@
  * Lodash is included as a bower.json dependency in case you would like to use it.
  */
 
-var arr = [1,2,5,7,4,8,12,15,17,37];
+(function(){       // Use IIFE to solve problem of Global Variables
+
+    'use strict';   // always use this statement at first line of Script
+
+    var arr = [1,2,5,7,4,8,12,15,17,37];
 var secondArr = [1,17,5,12,4,7,15,2,37];
+
+        /*How to solve this problem
+    First do the sorting of array and then loop through array and store value if index will be -1*/
+
+function findMissingNumber (first, second) {
+
+    var m;   // To store Missing Value
+
+    var sorted = first.sort(function (a, b) {
+        return a-b;
+    });            // Sort first Array
+
+    var sorted1 = second.sort(function (a, b) {
+        return a-b;
+    });          // sort second array
+
+    for (var i in sorted) {
+        if (sorted1.indexOf(sorted[i]) < 0) {
+            m = sorted[i];
+            break;
+        }
+    }
+    console.log("Number missing in second array is", m);  //Log final Result
+}
+findMissingNumber(arr, secondArr);
+})();
