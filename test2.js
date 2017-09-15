d343bc03b04027d46d7cf0db669e8edb39e9ab5e
ishit desai <desai_ishit@yahoo.com>
2017-09-15 19:25:26 -0400

    Solved Challenge-2

--- test2.js
+++ test2.js
@@ -7,19 +7,29 @@
  * Fix the function so that it returns [4,[1],[2],[3],[4]];
  */
 
-var count = 4;
-function foo() {
-  var table = [count];
-  while (count-- > 0) {
-    table.push([]);
-  }
-  for (var count = 1; count < table.length; count++) {
-    table[count].push(count);
+(function(){
+
+  'use strict';
+
+
+  var count = 4;
+
+  function foo(c) {
+
+  var table = [c];
+  /*while (count-- > 0) {         //Remove while loop to get desirable output
+      table.push([]);
+  }*/
+
+    // Here if we use count < table.length then it will go to infinite loop.
+
+    for (var i = 1; i <= c; i++) {
+    table.push([i]);
   }
   return table;
 }
+  console.log("Final Result is : ", foo(count));
 
-// console.log(foo());
 
 ////////
 // OR //
@@ -30,12 +40,21 @@ function foo() {
  * Explain why the code only logs -1, then fix the code so it works as expected.
  */
 
-function countdown (num) {
-  for (var i = 0; i <= num; i += 1) {
-    setTimeout(function () {
-      console.log(num - i);
-    }, i * 1000);
-  }
-}
 
-// countdown(5);
+  function countdown (num) {
+    // We can solve this problem in two ways
+    // 1) Use i using let keyword instead of var so that it can provide block scope
+    // 2) use IIFE (Immediately Invoked Function Expression) inside for loop and
+    //    pass i as a reference an argument to function.
+    //   so by using iife we can solve this problem by storing the value of i.
+
+    for (var i = 0; i <= num; i += 1) {
+      (function (i){
+        setTimeout(function () {
+          console.log(num - i);
+        }, i * 1000);
+      })(i);
+    }
+  }
+  console.log("Result is :", countdown(5));
+})();
